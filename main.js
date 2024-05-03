883c52588b1e13e75ca49ee162364a0c7bca91be
Illia <ilyusha.drozd.2016@gmail.com>
2024-05-03 14:10:02 +0300

    basic launch bot ready

---
+++ main.js
@@ -0,0 +1,9 @@
+import { Telegraf } from 'telegraf'
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
